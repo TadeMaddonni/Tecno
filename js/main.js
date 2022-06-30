c303ed68d094c1304afa9f51969c4e827fdc9f37
Tade Maddonni <tadeomaddonni@gmail.com>
2022-06-29 23:04:34 -0300

    agregando simulador js

---
+++ js/main.js
@@ -0,0 +1,118 @@
+
+let product = "";
+let price = 0;
+let priceConIva = 0;
+let agregarMas  = true;
+let finalPrice = 0;
+let otroIntento = false;
+let productName = product;
+
+const listaDeProductos = [];
+
+const suma = (a,b) => {return a + b}; // función de suma
+const resta = (a,b) => {return a - b}; //función de resta
+
+const iva = (x) => {return x * 1.21}; // cálculo del IVA
+const descuento = (a, b) => {return a * b /100}; // Calculo del descuento
+let descuentoFinal = 0
+
+function validarDatos (product, price){
+    if(product == "" || isNaN(price)){ //Validación de productos y precios validos
+        alert("error alguno de los datos ingresados son incorrectos");
+        otroIntento = prompt("¿Desea volver a intentar? si/no ").toLowerCase(); //Confirmación antes de finalzar
+
+        if (otroIntento == "si"){
+            product = prompt("Ingresa el producto a agregar en la lista");
+            price = Number(prompt("Ingrese el precio inicial del producto"));
+        } else{
+            alert("Programa finalzado");
+        }
+    }
+}
+
+
+// Función constructora de objetos
+
+class productos {
+    constructor (name, price,){
+        this.nombre = name,
+        this.precio = price
+    }
+}
+
+
+    do {
+        product = prompt("Ingresa el producto a agregar en la lista"); 
+        price = Number(prompt("Ingrese el precio inicial del producto"));
+
+        validarDatos(product,price);
+
+        priceConIva = iva(price); //Cálculo del IVA
+
+        let agregarDescuento = prompt("Desea agregarle un descuento al producto? si/no").toLowerCase();
+
+        if(agregarDescuento == "si" ){
+            agregarDescuento=true;
+        }else{
+                agregarDescuento=false;
+                porcentajeDescuento=0;
+            }
+        
+        if(agregarDescuento==true){ 
+            let porcentajeDescuento = Number(prompt("Ingrese el porcentaje de descuento que desea agregar, del 1 al 100"));
+            descuentoFinal =  descuento (price, porcentajeDescuento); // Cálculo para agregar descuento
+        }
+
+
+        finalPrice = resta(priceConIva, descuentoFinal); // Calculo del precio final
+        
+        // creando objeto y agregandolo al array
+        productName = new productos(product, price);
+        listaDeProductos.push(productName);
+
+
+        //Imprimiendo el producto y el precio del mismo en la página
+        alert(`${product} ha sido agregado a la lista con el precio de ${finalPrice}`);
+
+        agregarMas=confirm("¿Desea agregar mas productos?"); // Confirmación para continuar la compra
+
+        const nombresProductos = listaDeProductos.map(products => products.nombre);
+
+        if (agregarMas==false){
+            alert(`Hasta aqui llego la carga de stock`);
+            console.log(`------Hasta aqui llegó la carga de stock------`);
+            console.log(listaDeProductos);
+            console.log(nombresProductos);     
+            alert(`
+                Se agregaron correctamente los siguientes productos:
+                ${nombresProductos};
+            `)
+        }
+
+        descuentoFinal=0;
+        
+
+    } while (agregarMas);
+
+    let limitePrecio = 0;
+
+    let filtrarPorPrecio = confirm("¿Desea filtrar por precio?");
+    if (filtrarPorPrecio == true){
+        limitePrecio = Number(prompt("Ingrese el precio maximo"));
+        filtrar();
+    }
+
+
+
+    // Función de filtrado
+    function filtrar (){
+        const filtroPrecio =  listaDeProductos.filter( el => el.precio <= limitePrecio);
+        console.log(filtroPrecio);
+        
+        const preciosFiltrados = filtroPrecio.map(el => el.nombre);
+        alert(`
+                Los productos con precio menor a $${limitePrecio} son los siguientes:
+                ${preciosFiltrados}
+        `);
+    }
+
---
+++ js/stock.js
@@ -0,0 +1,241 @@
+
+const productos = [
+//Comienza carga de productos
+    {
+        id: 1,
+        nombre: "HyperX Cloud Core 7.1",
+        Img: "../assets/imgs/productos/auris/cloudcore.jpg",
+        tipo:"auris",
+        precio:12500,
+        cantidad:1,
+        desc: "asda",
+        carrito:`<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                </svg>`,
+    },
+
+    {
+        id: 2,
+        nombre:"HyperX Cloud II 7.1",
+        Img: "../assets/imgs/productos/auris/cloudii.jpg",
+        tipo: "auris",
+        precio:14000,
+        cantidad:1,
+        desc: "sad",
+        carrito:`<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                </svg>`,
+    },
+
+    {
+        id: 3,
+        nombre: "HyperX Cloud Alpha",
+        Img: "../assets/imgs/productos/auris/cloudalpha.jpg",
+        tipo: "auris",
+        precio: 12000,
+        cantidad: 1,
+        desc: "asasd",
+        carrito:`<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                </svg>`,
+    },
+
+    {
+        id: 4,
+        nombre: "HyperX Alpha Red",
+        Img: "../assets/imgs/productos/auris/cloudflights.jpg",
+        tipo: "auris",
+        precio: 12500,
+        cantidad: 1,
+        desc: "ads",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                </svg>`,
+    },
+
+    {
+        id: 5,
+        nombre: "HyperX cloud Flight S",
+        Img: "../assets/imgs/productos/auris/cloudflights.jpg",
+        tipo: "auris",
+        precio: 17000,
+        cantidad: 1,
+        desc: "asd",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                </svg>`,
+    },
+
+    {
+        id: 6,
+        nombre: "Logitech Gpro",
+        Img: "../assets/imgs/productos/auris/gprowireless.jpg",
+        tipo: "auris",
+        precio: 20000,
+        cantidad:1,
+        desc: "asd",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                </svg>`,
+    },
+
+    {
+        id: 7,
+        nombre: "LogiTech G435",
+        Img: "../assets/imgs/productos/auris/g435.jpg",
+        tipo: "auris",
+        precio: 15500,
+        cantidad:1,
+        desc: "asda",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+    {
+        id: 8,
+        nombre:"Logitech G333 KDA",
+        Img: "../assets/imgs/productos/auris/g333.jpg" ,
+        tipo: "auris",
+        precio:8500,
+        cantidad:1,
+        desc: "asda",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+    {
+        id: 9,
+        nombre: "HyperX Earbuds",
+        Img: "../assets/imgs/productos/auris/earbuds.jpg",
+        tipo: "auris",
+        precio: 8000,
+        cantidad:1,
+        desc: "asda",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+//Cambio de tipo de producto
+
+    {
+        id: 10,
+        nombre: "Ipad 10.2° 8va Gen",
+        Img: "../assets/imgs/productos/celus/ipad.jpg",
+        tipo: "cels",
+        precio: 60500,
+        cantidad:1,
+        desc: "dsa",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                </svg>`,
+    },
+
+    {
+        id: 11,
+        nombre: "Ipad Air 10.9 (green)",
+        Img: "../assets/imgs/productos/celus/ipadair.jpg",
+        tipo:"cels",
+        precio: 169000,
+        desc: "asds",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                </svg>`,
+    },
+
+
+    {
+        id: 12,
+        nombre: "Iphone 11 128GB",
+        Img: "../assets/imgs/productos/celus/iphoneonce.jpg",
+        tipo:"cels",
+        precio: 120000,
+        cantidad:1,
+        desc: "asdsa",
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+
+    {
+        id: 13,
+        nombre: "Samsung S 22 Ult Gen",
+        Img: "../assets/imgs/productos/celus/s22.jpg" ,
+        tipo: "cels",
+        precio:206500,
+        desc: "asd",
+        cantidad:1,
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+    {
+        id: 14,
+        nombre: "Samsung S21",
+        Img: "../assets/imgs/productos/celus/s21.jpg",
+        tipo: "cels",
+        precio: 169000,
+        desc: "sdsad",
+        cantidad:1,
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+    {
+        id: 15,
+        nombre: "Xiaomi 11",
+        Img: "../assets/imgs/productos/celus/xiaomi11.jpg",
+        tipo: "cels",
+        precio: 120000,
+        desc: "sad",
+        cantidad:1,
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+    {
+        id: 16,
+        nombre: "Xiaomi redmi note 10",
+        Img: "../../assets/imgs/productos/celus/xiaomi10.jpg",
+        tipo: "cels",
+        precio: 50000,
+        desc: "sadas",
+        cantidad:1,
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+    {
+        id: 17,
+        nombre: "Samsung galaxy Z Flip",
+        Img: "../../assets/imgs/productos/celus/samsungflip.jpg",
+        tipo: "cels",
+        precio:169000,
+        desc: "asdad",
+        cantidad:1,
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    },
+
+    {
+        id: 18,
+        nombre: "Iphone 12 Pro",
+        Img: "../../assets/imgs/productos/celus/iphonedoce .jpg",
+        tipo: "cels",
+        precio:180000,
+        desc: "asa",
+        cantidad:1,
+        carrito: `<svg class="aurisAddToCart" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+        </svg>`,
+    }
+//Termina carga de productos
+]
