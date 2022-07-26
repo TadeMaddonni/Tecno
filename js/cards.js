89dc96c25969fd095f8b7bffc5f9148a5e594240
Tade Maddonni <tadeomaddonni@gmail.com>
2022-07-25 21:56:25 -0300

    arreglos

--- js/cards.js
+++ js/cards.js
@@ -1,4 +1,9 @@
-import getData from "./getdata.js";
+const getData = async () => {
+    const response = await fetch("../js/data.json");
+    const data = await response.json();
+
+    return data
+}
 
 const mostrarProductos = async () => {
 
--- js/getdata.js
+++
@@ -1,9 +0,0 @@
-const getData = async () => {
-    const response = await fetch("/js/data/data.json");
-    const data = await response.json();
-
-    return data
-}
-
-
-export default getData
